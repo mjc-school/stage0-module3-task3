1c65062e3833b3feaf2c1fe99f10c33876722309
Oleg Zheleznov <oleg-zheleznov@mail.ru>
2022-06-16 13:00:52 +0300

    Fix task

--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -2,8 +2,5 @@ package lang.print.gaps.task3;
 
 public class DeclaringVars {
     public static void main(String[] args) {
-        int a, b, c;
-        a = b = c = 10;
-        System.out.println(c);
     }
 }
