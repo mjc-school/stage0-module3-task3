6179cc3f89e0f92d983c7d4e1c209a6b69dfa71a
Lazizbek <81162929+Lazizbek-code@users.noreply.github.com>
2022-10-24 16:30:06 +0500

    fix

--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -2,8 +2,7 @@ package lang.print.gaps.task3;
 
 public class DeclaringVars {
     public static void main(String[] args) {
-        int a, b, c;
-        a = 10; b = 10; c = 10;
+        int a = 10, b = 10, c = 10;
         System.out.println(c);
     }
 }
